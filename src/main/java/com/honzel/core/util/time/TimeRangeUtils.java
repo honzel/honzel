a99936f8b8a4748f6de90a69eb94ce0651c22f61
hongzhen.lu <aniu_zhengzhuan@163.com>
2026-09-18 11:45:56 +0800

    refactor(time): 优化时间范围工具类中的循环逻辑

--- src/main/java/com/honzel/core/util/time/TimeRangeUtils.java
+++ src/main/java/com/honzel/core/util/time/TimeRangeUtils.java
@@ -139,14 +139,13 @@ public class TimeRangeUtils {
         firstRange.setStartTime(parseTime((offset + firstStart) % TIME_BITS));
         // 获取第一个结束位
         int firstBits = Long.numberOfLeadingZeros(~times);
-        int firstEnd = firstStart + firstBits;
         if ((times >>>= firstBits) == NONE) {
             // 只有一个时间段
-            addEndTimeAndDivision(timeRangeList, firstRange, adjTime, adjStart, adjEnd, divisionDuration, halfDivisionDurationEnabled, shiftFlag, offset, firstEnd);
+            addEndTimeAndDivision(timeRangeList, firstRange, adjTime, adjStart, adjEnd, divisionDuration, halfDivisionDurationEnabled, shiftFlag, offset, firstStart + firstBits);
             return timeRangeList;
         }
         TimeRange timeRange = null;
-        for (int i = firstEnd; i < TIME_BITS; i ++, times >>>= 1) {
+        for (int i = firstStart + firstBits; i < TIME_BITS; i ++, times >>>= 1) {
             if (times == NONE) {
                 break;
             }
